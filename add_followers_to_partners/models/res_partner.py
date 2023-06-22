d68382e510eede047aea24e48eeba3f2b3537dd1
dani.xtendoo <dani.xtendoo@gmail.com>
2023-06-22 12:29:48 +0200

    sync

--- add_followers_to_partners/models/res_partner.py
+++ add_followers_to_partners/models/res_partner.py
@@ -7,7 +7,13 @@ class AddFollowers(models.Model):
     @api.model
     def add_followers_to_contacts(self):
         partner_ids = self.search([])
-        user_ids = self.env['res.users'].search([('active', '=', True)])
+        user_ids = self.env['res.users'].search([('id', 'in', (29, 34, 27, 31, 21, 33))])
         followers = self.search([('user_ids', 'in', user_ids.ids)])
         for partner in partner_ids:
             partner.sudo().message_subscribe(partner_ids=followers.ids)
+            partner.sudo().message_subscribe(partner_ids=partner.ids)
+
+    def delete_followers_to_contacts(self):
+        partner_ids = self.search([])
+        for partner in partner_ids:
+            partner.sudo().message_unsubscribe(partner_ids=partner_ids.ids)
