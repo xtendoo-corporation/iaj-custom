9b643acc46248127db02dfc93352900d20d93997
Dani <dani.xtendoo@gmail.com>
2023-11-07 11:23:31 +0100

    [MOD] Add_followers_to_partner, añadido el usuario con id 35

--- add_followers_to_partners/models/res_partner.py
+++ add_followers_to_partners/models/res_partner.py
@@ -7,7 +7,7 @@ class AddFollowers(models.Model):
     @api.model
     def add_followers_to_contacts(self):
         partner_ids = self.search([])
-        user_ids = self.env['res.users'].search([('id', 'in', (29, 34, 27, 31, 21, 33))])
+        user_ids = self.env['res.users'].search([('id', 'in', (29, 34, 27, 31, 21, 33, 35))])
         followers = self.search([('user_ids', 'in', user_ids.ids)])
         for partner in partner_ids:
             partner.sudo().message_subscribe(partner_ids=followers.ids)
